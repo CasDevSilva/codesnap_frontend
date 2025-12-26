2ec92bbab5b56b18e44c98597cc678dc3e1a0a30
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-25 23:46:02 -0500

    refactor(main): pasar props de estado a componentes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
 import Editor from './editor/Editor'
 import History from './History'
 
-const Main = ({ addSnippet, snippets, onClear }) => {
+const Main = ({
+    addSnippet,
+    snippets, onClear,
+    setShowSnippet,
+    setCurrentSnippet,
+    code, setCode,
+    language, setLanguage,
+    font, setFont,
+    theme, setTheme,
+    background, setBackground,
+    shadow, setShadow,
+    padding, setPadding
+
+}) => {
     return (
         <div className="w-full max-w-6xl mx-auto px-6 py-4">
             <div className="grid grid-cols-1 lg:grid-cols-[70%_30%] gap-6">
                 <Editor
                     addSnippet = { addSnippet }
+
+                    code       = {code}         setCode = {setCode}
+                    language   = {language}     setLanguage = {setLanguage}
+                    font       = {font}         setFont = {setFont}
+                    theme      = {theme}        setTheme = {setTheme}
+                    background = {background}   setBackground = {setBackground}
+                    shadow     = {shadow}       setShadow = {setShadow}
+                    padding    = {padding}      setPadding = {setPadding}
                 />
                 <History
-                    snippets = {snippets}
-                    onClear  = {onClear}
+                    snippets          = {snippets}
+                    onClear           = {onClear}
+                    setShowSnippet    = {setShowSnippet}
+                    setCurrentSnippet = {setCurrentSnippet}
                 />
             </div>
         </div>
